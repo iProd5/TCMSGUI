7774bb3f4c8a81c6763ebc2c370e7516e60d7d60
iProd5 <franklinluther3@gmail.com>
2019-12-04 09:56:00 +0000

    heroku deploy init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
 import { ProjectComponent } from './project/project.component';
 import { MainComponent } from './main/main.component';
+import { NewClientComponent } from './new-client/new-client.component';
+import { NewProjectComponent } from './new-project/new-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
     ProjectComponent,
-    MainComponent
+    MainComponent,
+    NewClientComponent,
+    NewProjectComponent
   ],
   imports: [
     BrowserModule,
