58bb0be8bcf0e95bd3f0c1a33076b00430266860
iProd5 <franklinluther3@gmail.com>
2019-12-04 16:19:10 +0000

    onit

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
+ clients = [
+
+  {id: '4', name: 'Samuel Kwakye', address: 'East Legon', email: 'kwayesamuel@gmail.com'},
+  {id: '5', name: 'Franklin Luther', address: 'Haatso', email: 'franklin@gmail.com'},
+  {id: '4', name: 'Samuel Kwakye', address: 'East Legon', email: 'kwayesamuel@gmail.com'},
+  {id: '5', name: 'Franklin Luther', address: 'Haatso', email: 'franklin@gmail.com' },
+  {id: '4', name: 'Samuel Kwakye', address: 'East Legon', email: 'kwayesamuel@gmail.com' },
+  {id: '5', name: 'Franklin Luther', address: 'Haatso', email: 'franklin@gmail.com'},
+  {id: '4', name: 'Samuel Kwakye', address: 'East Legon', email: 'kwayesamuel@gmail.com'},
+  {id: '5', name: 'Franklin Luther', address: 'Haatso', email: 'franklin@gmail.com'}
+  
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
