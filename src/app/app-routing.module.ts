cdb56c1f466895e35d959ae9399d843cbf49b9cb
iProd5 <franklinluther3@gmail.com>
2019-12-05 12:36:48 +0000

    solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {ClientComponent} from './client/client.component';
 import {MainComponent} from './main/main.component';
 import {ProjectComponent} from './project/project.component';
-import {NewprojectComponent} from './newproject/newproject.component';
-import {NewclientComponent} from './newclient/newclient.component';
+
 
 
 const routes: Routes = [
@@ -12,8 +11,7 @@ const routes: Routes = [
 {path:'project', component:ProjectComponent},
 {path:'main', component:MainComponent},
 {path:'client', component:ClientComponent},
-{path:'newclient', component:NewclientComponent},
-{path:'newproject', component:NewprojectComponent}
+
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule }    from '@angular/common/http';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
 import { ProjectComponent } from './project/project.component';
 import { MainComponent } from './main/main.component';
-import { NewClientComponent } from './new-client/new-client.component';
-import { NewProjectComponent } from './new-project/new-project.component';
-import { ClientlinkComponent } from './clientlink/clientlink.component';
-import { ProjectlinkComponent } from './projectlink/projectlink.component';
-import { CustomerFormComponent } from './customer-form/customer-form.component';
-import { NewclientComponent } from './newclient/newclient.component';
-import { ProjectclientComponent } from './projectclient/projectclient.component';
-import { NewprojectComponent } from './newproject/newproject.component';
+
 
 
 @NgModule({
@@ -23,14 +16,7 @@ import { NewprojectComponent } from './newproject/newproject.component';
     ClientComponent,
     ProjectComponent,
     MainComponent,
-    NewClientComponent,
-    NewProjectComponent,
-    ClientlinkComponent,
-    ProjectlinkComponent,
-    CustomerFormComponent,
-    NewclientComponent,
-    ProjectclientComponent,
-    NewprojectComponent
+
   ],
   imports: [
     BrowserModule,
